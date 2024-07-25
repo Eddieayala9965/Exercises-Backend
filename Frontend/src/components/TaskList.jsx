105dfb1bfff2c8fdd7ee44c08699f3515a63e2bc
Eddie Ayala <heriberto.ayala1981@gmail.com>
2024-07-25 17:31:10 -0400

    need food

--- Frontend/src/components/TaskList.jsx
+++ Frontend/src/components/TaskList.jsx
@@ -2,6 +2,12 @@ import { useEffect, useState } from "react";
 import { getTasks } from "../services/api";
 
 const TaskList = () => {
+  const [tasks, setTask] = useState([]);
+
+  useEffect(() => {
+    const fetchTasks
+  }, []);
+
   return (
     <>
       <div></div>
