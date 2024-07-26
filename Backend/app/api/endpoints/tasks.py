203fbadc9a159bfb979940971549cc86b5ac8a0c
Eddie Ayala <heriberto.ayala1981@gmail.com>
2024-07-26 16:25:09 -0400

    added small changes

--- Backend/app/api/endpoints/tasks.py
+++ Backend/app/api/endpoints/tasks.py
@@ -6,19 +6,19 @@ from ...database import get_db
 
 router = APIRouter()
 
-@router.post("/tasks/", response_model=schemas.Task)
+@router.post("/create_tasks/", response_model=schemas.Task)
 def create_task(task: schemas.TaskCreate, db: Session = Depends(get_db)):
     return crud.create_task(db=db, task=task)
 
-@router.get("/tasks/", response_model=List[schemas.Task])
+@router.get("/get_tasks/", response_model=List[schemas.Task])
 def read_task(skip: int = 0, limit: int = 10, db: Session = Depends(get_db)):
     tasks = crud.get_tasks(db, skip=skip, limit=limit)
     return tasks
 
-@router.delete("/tasks/{task_id}", response_model=schemas.Task)
+@router.delete("/delete_tasks/{task_id}", response_model=schemas.Task)
 def delete_task(task_id: int, db: Session = Depends(get_db)):
     return crud.delete_task(db=db, task_id=task_id)
 
-@router.put("/tasks/{task_id}", response_model=schemas.Task)
+@router.put("/update_tasks/{task_id}", response_model=schemas.Task)
 def update_task(task_id: int, task: schemas.TaskUpdate, db: Session = Depends(get_db)):
     return crud.update_task(db=db, task_id=task_id, task=task)
